a3965652f9a5945c1a55c2e464918348ec3d2954
paramasivam2709 <paramasivamb2000@gmail.com>
2026-01-24 18:27:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-const btn = document.getElementById("payBtn");
-const success = document.getElementById("success");
+const tabs = document.querySelectorAll(".tab");
+  tabs.forEach(tab=>{
+    tab.addEventListener("click",()=>{
+      tabs.forEach(t=>t.classList.remove("active"));
+      tab.classList.add("active");
+    });
+  });
 
-btn.onclick = () => {
-  if(!amount.value) return;
-  success.classList.remove("hidden");
-};
+  // Fade-in animation on scroll
+const cards = document.querySelectorAll(".refer-card");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+},{ threshold:0.2 });
+
+cards.forEach(card=>{
+  card.style.opacity = 0;
+  card.style.transform = "translateY(40px)";
+  card.style.transition = "0.6s ease";
+  observer.observe(card);
+});
+
+
